97551e89832c091c4eb710af616ce8a3cb8ff8aa
U4RIA1028 <102589234+U4RIA1028@users.noreply.github.com>
2026-03-16 21:58:48 +0900

    Update PaintPuzzle.cpp

--- Capstone/Source/Object/PaintPuzzle.cpp
+++ Capstone/Source/Object/PaintPuzzle.cpp
@@ -3,63 +3,12 @@
 
 #include "Object/PaintPuzzle.h"
 
-APaintPuzzle::APaintPuzzle()
-{
-	PrimaryActorTick.bCanEverTick = true;
-
-	Paint = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("PaintMesh"));
-	RootComponent = Paint;
-
-	Box = CreateDefaultSubobject<UBoxComponent>(TEXT("ClickableBox"));
-	Box->SetupAttachment(RootComponent);
-	Box->SetBoxExtent(FVector(10.f, 10.f, 10.f)); // Äİ¸®Àü ¹Ú½ºÀÇ Å©±â ¼³Á¤
-	Box->SetRelativeLocation(FVector(0.f, 0.f, -75.f)); // Äİ¸®Àü ¹Ú½ºÀÇ À§Ä¡ ¼³Á¤
-	Paint->SetCollisionProfileName("IgnoreOnlyPawn");
-
-	static ConstructorHelpers::FObjectFinder<UStaticMesh> PaintMeshAsset(TEXT("/Script/Engine.StaticMesh'/Game/HorrorGamePuzzlePack/Models/PaintingPuzzle/Mesh/PaintingPuzzle_SM.PaintingPuzzle_SM'"));
-	if (PaintMeshAsset.Succeeded())
-	{
-		Paint->SetStaticMesh(PaintMeshAsset.Object);
-	}
-
-	bIsPuzzleSolved = false;
-
-}
-
-void APaintPuzzle::OnConstruction(const FTransform& Transform)
-{
-	Super::OnConstruction(Transform);
-	if (Paint && PictureMaterial)
-	{
-		Paint->SetMaterial(3, PictureMaterial);
-	}
-}
-
-void APaintPuzzle::BeginPlay()
-{
-	Super::BeginPlay();
-}
-
-
-void APaintPuzzle::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-}
-
-void APaintPuzzle::Init()
-{
-	ObjectInfo->set_object_type(Protocol::ObjectType::OBJECT_IDLE);
-
-	Super::Init();
-}
-
-
 bool APaintPuzzle::OnClicked(class ACapstoneMyPlayer* Player)
 {
 	UE_LOG(LogTemp, Warning, TEXT("Interact!"))
 	if (bIsPuzzleSolved)
 	{
-		return true; // ÆÛÁñÀÌ ÇØ°áµÈ »óÅÂ¿¡¼­´Â ´õ ÀÌ»ó Å¬¸¯À» Ã³¸®ÇÏÁö ¾ÊÀ½
+		return true; // í¼ì¦ì´ í•´ê²°ëœ ìƒíƒœì—ì„œëŠ” ë” ì´ìƒ í´ë¦­ì„ ì²˜ë¦¬í•˜ì§€ ì•ŠìŒ
 	}
 
 	if (PaintManager)
